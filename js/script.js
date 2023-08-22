b321fb22d85ae119e77a718b9c7a00e87c351303
yinianyangliu <1412969302@qq.com>
2023-08-22 19:39:14 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 var index = 0
 function oy_go_next() {
-    console.log(listItems)
+    console.log(end0)
+    end0.style.display = "block"
+
+    var yesorno = document.getElementById("ul_oy_btn1")
+    yesorno.style.display = "none"
+    end1.style.display = "none"
 }
 function oy_show_benefit() {
     if (index >= listItems.length) {
-        var yesorno = document.getElementById("ul_oy_btn2")
-           yesorno.style.display = "block"
-        return
+        end1.style.display = "block"
+        var btn1 = document.getElementsByClassName("btn")
+        btn1[1].style.display = "none"
+        return 
     }
+
     listItems[index].style.display = "block"
     index += 1
 }
